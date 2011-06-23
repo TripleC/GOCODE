87e455114f34de036f18623155cf46af632fbcd9
TripleC <losehole@gmail.com>
2011-06-23 15:07:03 +0800

    GO code

---
+++ webhello.go
@@ -0,0 +1,10 @@
+package main
+import "fmt"
+import "http"
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprint(w, "Hello, world")
+}
+func main() {
+    http.HandleFunc("/", handler)
+    http.ListenAndServe(":8080", nil)
+}
